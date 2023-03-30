3cb2e54777d53ce825a11a2cd7c895c8c8a18ca2
Duke <aboukhodr21@gmail.com>
2023-03-29 20:01:40 -0400

    gotrunning

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mongoose from 'mongoose'
+mongoose.connect(process.env.MONGO_URL);
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+export default async function handler(req, res) {
+  const Book = mongoose.model('Book');
+
+  const never = new Book({ author: "Jeff Bezos", title: "Invent And Wander" });
+  await never.save();
+  const neverland = await Book.findOne({author: "Jeff Bezos"}).exec()
+  res.status(200).json(neverland)
 }
