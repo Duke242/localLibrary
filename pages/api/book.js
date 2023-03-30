5161540d040b7106a76d67cfc29387f0c41b0b97
Duke <aboukhodr21@gmail.com>
2023-03-29 23:39:09 -0400

    working GET and POST

--- pages/api/book.js
+++ pages/api/book.js
@@ -1,11 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // import mongoose from "../../lib/mongoose";
+import { setup } from '@/lib/mongoose';
 import mongoose from 'mongoose'
 
 
 
 export default async function handler(req, res) {
   mongoose.connect(process.env.MONGO_URL);
+  setup(mongoose)
   const Book = mongoose.model('Book');
   
   if (req.method === 'GET') { 
